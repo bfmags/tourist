290b48b405b9d06c1b657c6ce7f71f33e49cb538
Tom Rosier <tom.rosier@bbc.co.uk>
2016-01-29 11:05:39 +0000

    now auto rebuilds and reloads page on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,10 @@ module.exports = function(grunt) {
     watch: {
       build: {
         files: ['src/**/*.coffee'],
-        tasks: ['concat', 'coffee:build']
+        tasks: ['concat', 'coffee:build'],
+        options: {
+          livereload: true,
+        },
       },
       test: {
         files: ['test/src/**/*.coffee'],
@@ -46,12 +49,22 @@ module.exports = function(grunt) {
       }
     },
 
+    parallel: {
+      grunt: {
+         options: {
+          grunt: true
+        },
+        tasks: ['watch', 'serve']
+      },
+    },
+
     jasmine: {
       src: '<%= pkg.name %>.js',
       options: {
         specs: ['test/lib/tourSpec.js', 'test/lib/tip/baseSpec.js', 'test/lib/tip/qtipSpec.js', 'test/lib/tip/bootstrapSpec.js'],
         helpers: 'test/lib/**/*Helper.js',
-        vendor: ['test/vendor/javascripts/jquery-1.9.1.js',
+        vendor: [
+          'test/vendor/javascripts/jquery-1.9.1.js',
           'test/vendor/javascripts/underscore-1.4.4.js',
           'test/vendor/javascripts/backbone-1.0.0.js',
           'test/vendor/javascripts/jquery.qtip.js',
@@ -59,14 +72,23 @@ module.exports = function(grunt) {
           'test/vendor/javascripts/jasmine-jquery.js'
         ]
       }
+    },
+    serve: {
+        options: {
+            port: 9000
+        }
     }
+
+
   });
 
+  grunt.loadNpmTasks('grunt-parallel');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-serve');
 
   grunt.registerTask('test', ['concat', 'coffee', 'jasmine']);
   grunt.registerTask('default', ['concat', 'coffee', 'uglify']);
